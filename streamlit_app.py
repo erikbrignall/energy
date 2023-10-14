d2443c0378c1ac62c4df21c1719a0855f5fc0d76
Erik Brignall <Erik.Brignall@rb.com>
2023-10-14 17:26:41 +0100

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,19 @@ import requests
 import time
 import json
 import streamlit as st
+from embedchain import App
 
 st.set_page_config(page_title='National Grid Analyst')
 st.image('logo-temp2.PNG', width=200)
 st.title('ChasBot 1000 - Grid Analyst')
-st.write('The following is a demo of the using LLMs to analy')
+st.write('The following is a demo of the using LLMs to analyse national grid data')
 
 openai.api_key = st.secrets["OpenAIapikey"]
+
+url = "https://api.nationalgrideso.com/dataset/cbd45e54-e6e2-4a38-99f1-8de6fd96d7c1/resource/17becbab-e3e8-473f-b303-3806f43a6a10/download/tec-register-13-10-2023.csv"
+
+df = pd.read_csv(url)
+df.head()
 
 # Input Query
 with st.form(key='my_form_to_submit'):
@@ -48,36 +54,6 @@ if submit_button:
 #if query_text is not None:   
         print("we have a query now")
         # FETCH RESPONSE
-
-        messages=[
-        {"role": "system", "content": "You are a data analyst who needs to take a request for a chart and convert that into specific parameters which will be used to generate the chart.\
-        The parameters which need returning are: \
-        name: a short descriptive title for the chart,\
-        type: bar/pie/line/bubble/table/tree map,\
-        dimension: date/location/browser/OS/Device/Browser/Channel/UserType(New or Returning Users)/Country/Region/City/Source/Campaign/Medium/Page/Landing Page/Product/Category/Brand/Landing Page/Lead Type,\
-        breakdown dimension: where the chart type is a line or bar chart that uses date as the dimension the breakdown dimension will be one of the other dimensions,\
-        y_axis_key: Sessions/ Effective sessions/ Users/ Revenue/ Transactions/ CVR/ Time on site/ Engagement rate/ROI,\
-        Effective: Where effective is mentioned in the request for a chart this should be Y else N, \ 
-        StartDate: [yyyy-mm-dd], \
-        End Date: [yyyy-mm-dd], \
-        Filter: if a filter is require specify dimension of filter,\
-        Filter Quantity: If a filter is based on a number specify this,\
-        Filter Direction: If a filter is used specify order of filter ascending or descending,\
-        Chart Template: A template is selected depending on the type of chart returned. Templates are Trend, for any time based charts, Table for tables and if neither of these is appropriate default can be set,\
-        Todays date is 2023/10/01. Where a date range is not specified use a start date of 31 days ago and end date of yesterday,\
-        The response should be a JSON object of these parameters.\
-        When a line or bar chart is requested, the default dimension should be date, and the extra parameter included for the breakdown dimension called BreakdownDimension.\
-        When two metrics are used in a bar or line chart the y_axis_key can show two metrics seperated by commas.\
-        When a table requires multiple dimensions or metrics to be defined, these can be listed in a comma seperated format with a maximum of 2 dimensions and 4 metrics returned.\
-        The dimension parameter and y_axis_key must always contain at least 1 value."},\
-        {"role": "user", "content": query_text}
-        ]
-        
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            temperature = 0.1,
-            stop = None,
-            messages=messages)
         
         
         response_text = response.choices[0].message.content
@@ -87,20 +63,4 @@ if submit_button:
         st.write("Here is the JSON for the chart generation:")
         st.json(LLMresponse)
         
-        ## extract individual elements from response
         
-        ## TITLE
-        #pattern = r'title: (.*?)summary:'
-        #match = re.findall(pattern, response_text)
-        #art_title = match[0]
-        
-        ## SUMMARY
-        #pattern = r'summary: (.*?)places:'
-        #match = re.findall(pattern, response_text)
-        #art_summary = match[0]
-        
-        ## PLACES
-        #pattern = r'places: (.*)'
-        #match = re.findall(pattern, response_text)
-        #places = match[0]
-        #placeurl = quote(places)
